7636d72317a2ee4a27b73b3b13598ddd860094e3
Mujo Ramic <32583467+MrNoOne007@users.noreply.github.com>
2018-05-18 18:19:25 +0200

    Update Korisnici.java

--- DTO/Korisnici.java
+++ DTO/Korisnici.java
@@ -1,5 +1,46 @@
 package DTO;
 
-public class Korisnici {
-
-}
+	package DTO;
+	
+	public class Korisnici {
+    
+		// data fields
+		private int userID;
+		private String userName;
+		public int getUserID() {
+			return userID;
+		}
+	
+		public void setUserID(int userID) {
+			this.userID = userID;
+		}
+	
+		private String password;
+	
+		public Korisnici() {
+	
+		}
+	
+		public Korisnici(int userID, String userName, String password) {
+			this.userID =userID;
+			this.userName = userName;
+			this.password = password;
+		}
+	
+		public String getUserName() {
+			return userName;
+		}
+	
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+	
+		public String getPassword() {
+			return password;
+		}
+	
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	
+	}
