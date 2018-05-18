3bcd206dfe250c795c2a98c8656edff192932843
Mujo Ramic <32583467+MrNoOne007@users.noreply.github.com>
2018-05-18 18:18:07 +0200

    Update ImenikDAOInterface.java

--- DAO/ImenikDAOInterface.java
+++ DAO/ImenikDAOInterface.java
@@ -1,5 +1,26 @@
 package DAO;
 
+import java.sql.SQLException;
+
+
 public interface ImenikDAOInterface {
+// metoda za dodavanje korisnika
+		
+	public void add(String Korisnik) throws SQLException;
+	
+	//metoda za azuriranje
+
+	public void edit(String Korisnik) throws SQLException;
+	
+	//metoda za brisanje korisnika
+
+	public void delete( ) throws SQLException;
+	
+	// metoda za pregled korisnika
+
+	public  void viewAll(String Korisnik) throws SQLException;
 
 }
+	
+	
+
