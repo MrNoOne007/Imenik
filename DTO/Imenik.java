ebafcc723ca792f767b398e85f7a3f3b9657439e
Mujo Ramic <32583467+MrNoOne007@users.noreply.github.com>
2018-05-18 18:20:18 +0200

    Update Imenik.java

--- DTO/Imenik.java
+++ DTO/Imenik.java
@@ -1,5 +1,44 @@
 package DTO;
 
+
+
 public class Imenik {
 
+// data fields
+	
+	private String name;
+	private String lastname;
+	private String phoneNum;
+	
+	
+	public Imenik (String name, String lastname, String phoneNum) {
+		this.name = name;
+		this.lastname = lastname;
+		this.phoneNum = phoneNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}	
 }
+
